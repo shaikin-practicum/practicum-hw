9b945ad4d7706ee0ad6e2ccd975d715f55eab7b8
sergeisrhaikin <shaikins@yahoo.com>
2024-05-07 20:28:22 +0200

    Implemented test case Search bar verification

--- features/Test.feature
+++ features/Test.feature
@@ -1,6 +1,9 @@
 # Created by sergeishaikin at 03.05.2024
-Feature: Hello World
+Feature: eBay Regression
   # Enter feature description here
 
-  Scenario: Test
-    Given Navigate to Google
+  Scenario: Search bar verification
+    Given Navigate to ebay
+    And In search bar type "shoes"
+    And Click "search" button
+    Then All displayed items are relevant to keyword "shoes"
--- features/steps/definitions.py
+++ features/steps/definitions.py
@@ -1,8 +1,27 @@
 from behave import step
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
-@step('Navigate to Google')
+@step('Navigate to ebay')
 def test(context):
-    browser = webdriver.Chrome()
-    browser.get("https://www.google.com")
+    context.browser = webdriver.Chrome()
+    context.browser.maximize_window()
+    context.browser.get("https://www.ebay.com")
+
+
+@step('In search bar type "shoes"')
+def search_items(context):
+    search = context.browser.find_element(By.XPATH, "//input[@placeholder = 'Search for anything']")
+    search.send_keys("shoes")
+
+
+@step('Click "search" button')
+def click_search(context):
+    press_search = context.browser.find_element(By.XPATH, "//input[@type = 'submit']")
+    press_search.click()
+
+
+@step('All displayed items are relevant to keyword "shoes"')
+def verify_results_shoes_items(context):
+    results = context.browser.find_element(By.XPATH, "//span[contains(text(), 'Shoes')]")
