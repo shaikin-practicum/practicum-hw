8456022418bc830c32e6a30fd9ecbc42a396cada
sergeisrhaikin <shaikins@yahoo.com>
2024-05-29 20:47:52 +0200

    Added after_step

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,20 @@
+import os
 from selenium import webdriver
 
 
-def before_scenario(context, scenario):
+def before_feature(context, feature):
     context.browser = webdriver.Chrome()
     context.browser.maximize_window()
+
+
+def before_scenario(context, scenario):
     context.browser.get("https://www.ebay.com")
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        current_dir = os.path.dirname(__file__)
+        print(current_dir)
+        relative_path_to_dest = os.path.abspath(os.path.join(current_dir, 'failed_screenshots'))
+        print(relative_path_to_dest)
+        context.browser.save_screenshot(os.path.join(relative_path_to_dest, f'{step.name}.png'))
