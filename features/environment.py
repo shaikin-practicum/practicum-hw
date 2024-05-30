8dd76d3df5677eab2672fe7ef018ed858a55b1f5
sergeisrhaikin <shaikins@yahoo.com>
2024-05-30 19:32:52 +0200

    Added creating folder for screenshots

--- features/environment.py
+++ features/environment.py
@@ -14,7 +14,7 @@ def before_scenario(context, scenario):
 def after_step(context, step):
     if step.status == 'failed':
         current_dir = os.path.dirname(__file__)
-        print(current_dir)
         relative_path_to_dest = os.path.abspath(os.path.join(current_dir, 'failed_screenshots'))
-        print(relative_path_to_dest)
+        if not os.path.exists(relative_path_to_dest):
+            os.mkdir(relative_path_to_dest)
         context.browser.save_screenshot(os.path.join(relative_path_to_dest, f'{step.name}.png'))
